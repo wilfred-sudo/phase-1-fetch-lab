5905e01db2f642e8341fde6cabbf75a0b077beb5
wilfredkiprop <kipropwilfred90@gmail.com>
2025-04-02 11:33:23 +0300

    fetch

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  if (!main){
+    console.error("Error: No element found in the DOM.");
+    return;
+  }
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
